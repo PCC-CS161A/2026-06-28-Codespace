367aaa1a56fbbcf0d00b2f9c87df8d71ac2f5790
PCC-CS161A <luis.hernandez14@pcc.edu>
2026-06-28 22:26:40 -0700

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,54 @@
+// ------------- FILE HEADER -------------
+// Author ✅: 
+// Assignment ✅:
+// Date ✅:
+// Citations: 
+
+
+// ------------- CODE -------------
+#include <iostream>
+
+using namespace std;
+
+// Function prototypes (if any)
+
+
+// Main function
+// https://en.cppreference.com/w/cpp/language/main_function.html
+int main() {
+  cout << "Hello, World!" << endl;
+  return 0;
+}
+
+// Function implementations (if any)
+
+
+// ------------- DESIGN -------------
+/* 
+Program Name:
+
+Program Description:
+
+Design:
+A. INPUT
+Define the input variables including name data type. 
+
+B. OUTPUT
+Define the output variables including data types. 
+
+C. CALCULATIONS
+Describe calculations used by algorithms in step D.  
+List all formulas. 
+If there are no calculations needed, state there are no calculations.
+
+D. LOGIC and ALGORITHMS
+Design the logic of your program using pseudocode or flowcharts. 
+Use conditionals, loops, functions or array constructs.
+List the steps in transforming inputs into outputs. 
+https://github.com/Glen-Sasek-PCC-Instructor/2025-06-22/blob/main/Pseudocode-Reference.txt
+
+
+SAMPLE RUNS
+Copy from assignment document.
+
+*/
